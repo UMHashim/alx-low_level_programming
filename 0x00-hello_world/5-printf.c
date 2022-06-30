4ca336153618306c49ad9b6b4a591a59d7ada448
UMHashim <niger.4life@gmail.com>
2022-06-30 15:22:48 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point
- *
- * description of main: qoute
- *
- * return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
 }
