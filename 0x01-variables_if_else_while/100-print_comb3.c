fc59ef2e0c371dc7b581b0646ec977bfa17cb001
UMHashim <niger.4life@gmail.com>
2022-07-03 14:41:45 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - assign a random number to the variable n each time it is executed
+ * and to print the last digit of the number stored in the variable n
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int ch;
+	int n;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			if (n > ch)
+			{
+				putchar(ch);
+				putchar(n);
+				if (ch != 56 || n != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	/* this is an ASCII code for this line */
+	return (0);
+}
