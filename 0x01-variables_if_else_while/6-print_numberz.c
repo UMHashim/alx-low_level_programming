d4a07300b682d9e38110306dd42fadc767f1a297
UMHashim <niger.4life@gmail.com>
2022-07-03 02:29:02 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	/* this is an ASCII code for this line */
+	return (0);
+}
